6e91f67c006971cef12de0258221ac73a5d88e34
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-07-26 15:20:16 +0200

    added class exports

--- lib/duck_adapter.dart
+++ lib/duck_adapter.dart
@@ -1,3 +1,9 @@
 library duck_adapter.library;
 
+import 'package:logging/logging.dart';
+
 ///exports classes which should be visible to the public
+
+export "src/session.dart";
+export 'src/load_options.dart';
+export 'src/sync_options.dart';
