21c4fc742ea45e29d53a582eb7a29e6737870fa5
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-07-26 22:44:16 +0200

    added server copy and remote file edit, as well as url context for path

--- lib/src/duck_process.dart
+++ lib/src/duck_process.dart
@@ -1,13 +1,22 @@
 library duck_adapter.duck_process;
 
 import 'dart:async';
+import 'dart:io';
 import 'sync_options.dart';
 import 'load_options.dart';
 import 'package:logging/logging.dart';
 import 'package:path/path.dart' as path;
 
 class DuckProcess {
-  final Logger _log = new Logger("duck_adapter.session");
+  final Logger _log = new Logger("duck_adapter.duck_process");
+
+  ///gives the stdout of duck for each duck call to the attached stream
+  ///each event on this stream will be a complete stdout of one duck call
+  final StreamController<String> stdout_controller = new StreamController<String>.broadcast();
+
+  Stream<String> get stdout => stdout_controller.stream;
+
+  path.Context _url;
 
   ///path to root folder of remote server
   ///e.g. ftp://<hostname>/
@@ -23,43 +32,86 @@ class DuckProcess {
   ///NOTE: Password auth is not supported right now
   String identityFile;
 
+  bool quiet;
+
+  ///prints transcript on stdout
+  bool verbose;
+
+  ///assumes yes for all promts
+  bool assume_yes;
+
   //TODO: Support password authentication as secure as possible
 
-  DuckProcess(this.remoteRoot, {this.user: "", this.externalEditor: "", this.identityFile: ""}) {
-    if (!path.isAbsolute(remoteRoot))
+  DuckProcess(this.remoteRoot,
+      {this.user: "", this.externalEditor: "", this.identityFile: "",
+      this.quiet: true, this.verbose: false, this.assume_yes: true}) {
+    //
+    //
+
+    if (path.url.isRelative(remoteRoot) || path.url.isRootRelative(remoteRoot))
       throw new path.PathException("remoteRoot is not absolute");
-
-    if (!remoteRoot.endsWith("/"))
-      remoteRoot += "/";
+    if (!remoteRoot.endsWith("/")) remoteRoot += "/";
+    _url = new path.Context(style: path.Style.url, current: remoteRoot);
   }
 
-  Future upload(String remotePath, String localPath, {LoadOptions handleExisting}) async {
+  Future upload(String remotePath, String localPath, {LoadOptions handleExisting}) async {}
 
-  }
+  Future download(String remotePath, String localPath, {LoadOptions handleExisting}) async {}
 
-  Future download(String remotePath, String localPath, {LoadOptions handleExisting}) async {
+  Future sync(String remotePath, String localPath, {SyncOptions handleExisting}) async {}
 
-  }
+  Future delete() async {}
 
-  Future sync(String remotePath, String localPath, {SyncOptions handleExisting}) async {
-
-  }
-
-  Future delete() async {
-
-  }
-
+  ///Copy between servers
   ///remote location 1 = absolute | relative to this.remoteRoot
   ///remote location 2 = absolute | relative to this.remoteRoot
-  Future serverCopy(remoteLocation1, remoteLocation2) async {
+  Future serverCopy(String remoteLocation1, String remoteLocation2) async {
 
+    //_url context knows root url -> matching is not necessary
+
+    var args = [
+      "--copy", _url.absolute(remoteLocation1), _url.absolute(remoteLocation2)
+    ];
   }
 
   ///Opens the given file in an external editor
-  ///Uses optionalParameter first and this.externalEditor secondly
+  ///Uses optional parameter  externalEditor first and this.externalEditor secondly
   Future editRemoteFile(String remoteFile, {String externalEditor}) async {
+    var editor = externalEditor ?? this.externalEditor;
+    if (editor.isEmpty || !(new File(editor).existsSync()))
+      throw new Exception(
+          "No external editor specified. Please specify external editor with optional param in editRemoteFile"
+              " or set external editor at DuckProcess construction time");
 
+    var args = [
+      "--application", editor,
+      "--edit", remoteFile
+    ];
+
+    return runDuck(args);
   }
 
+  ///only to be feature complete
+  Future<String> get version async {
+    ProcessResult processResult = await runDuck(["--version"], addSessionParams: false);
+    return processResult.stdout;
+  }
 
+  Future<ProcessResult> runDuck(List<String> args, {bool addSessionParams: true}) {
+    ///add session params
+    if (addSessionParams) {
+      if (quiet)
+        args.add("--quiet");
+
+      if (verbose)
+        args.add("--verbose");
+
+      if (assume_yes)
+        args.add("--assumeyes");
+    }
+
+    _log.fine(args.join(" "));
+
+    return Process.run("duck", args);
+  }
 }
